d7823e9193f5a1cd738b54df0eb3827bc2270b70
Daniel Gafni <danielgafni16@gmail.com>
2020-12-04 21:36:20 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="masters",
+      version="0",
+      description="master\'s thesis",
+      author='Daniel Gafni',
+      author_email="danielgafni16@gmail.com",
+      packages=["masters"],
+      requires=["wheel"]
+)
